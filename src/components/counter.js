3af42134f0433d846d43f91333abf53b4736faba
aimanalii <aimanzahid828@gmail.com>
2021-04-14 10:51:10 +0500

    connector Added

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,40 +1,44 @@
 import React from 'react';
-import {connect} from 'react-redux';
-class Counter extends React.Component{
-    constructor(){
+//import reducer from '../store/reducer'
+import { connect } from 'react-redux';
+class Counter extends React.Component {
+    constructor() {
         super();
-    this.state={
-        count: 0
+        this.state = {
+            count: 0
+        }
     }
-}
- counterChangeHandler(action , value=1){
-     let current= this.state.count;
-     if(action === "inc")
-      current= current + value;
-     else
-      current= current - value;
-    this.setState({
-        count:current
-    })
- }
-    render(){
-        return(
+    counterChangeHandler(action, value = 1) {
+        let current = this.state.count;
+        if (action === "inc")
+            current = current + value;
+        else
+            current = current - value;
+        this.setState({
+            count: current
+        })
+    }
+    render() {
+        console.log("CTR", this.props.ctr)
+        return (
             <div>
-                <h1>Total count is: {this.props.ctr} . </h1>
+                <h1>Total count is: {this.state.count} . </h1>
 
-                <button onClick={()=>this.props.onIncCounter}>
+                <button onClick={() => {
+                    this.props.onIncCounter()
+                    }}>
                     Inc
                 </button>
 
-                <button onClick={()=>this.counterChangeHandler("dec")}>
+                <button onClick={() => this.counterChangeHandler("dec")}>
                     Dec
                 </button>
 
-                <button onClick={()=>this.counterChangeHandler("inc", 5)}>
+                <button onClick={() => this.counterChangeHandler("inc", 5)}>
                     Add 5
                 </button>
 
-                <button onClick={()=>this.counterChangeHandler("dec", 5)}>
+                <button onClick={() => this.counterChangeHandler("dec", 5)}>
                     Sub 5
                 </button>
             </div>
@@ -42,14 +46,14 @@ class Counter extends React.Component{
     }
 }
 const mapStateToProps = state => {
-    return{
-      ctr: state.count 
+    return {
+        ctr: state.counter
     };
-    
+
 };
 const mapDispatchToProps = dispatch => {
-    return{
-        onIncCounter : () => dispatch({ type: 'INCREMENT'})
+    return {
+        onIncCounter: () => dispatch({ type: 'INCREMENT' })
     };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
